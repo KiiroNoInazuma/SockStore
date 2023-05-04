90afda0a37e0cae555979060723dfc53181817ae
John Rey <adioskid@mail.ru>
2023-05-04 16:00:09 +0300

    MyApp

---
+++ src/main/java/com/example/sockstore/SockStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.sockstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sockstore/SockStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SockStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
